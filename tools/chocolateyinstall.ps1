78885d938c10b7f1b5717a201378e7484e4ffc41
Cthulhux <Cthulhux@noemail.net>
2020-03-13 07:46:22 +0000

    Update: pkg ver
    FossilOrigin-Name: 20ca93e6dec6880d246e71d9c1a067102fb6ee2fcc86cebfe8ebf9bae9692d87

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,5 +1,5 @@
 ﻿$packageName        = 'WordTsar'
-$version            = '0.9.6'
+$version            = '0.9.7'
 $url                = "https://sourceforge.net/projects/wordtsar/files/Releases/WordTsar-0.1.1977/WordTsar-Windows-0.1.1977.zip/download?use_mirror=autoselect"
 $checksum           = "0EE9957109399C92A5B22535F28F133A914A257B9D4F8FB99113C0CA43D86262"
 
